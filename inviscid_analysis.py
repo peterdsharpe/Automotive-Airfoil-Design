03cedac14b591601fcbabe20e850ff8e15e3e497
Peter Sharpe <peterdsharpe@gmail.com>
2021-01-23 01:19:48 -0500

    final cleanup of inviscid analysis

--- inviscid_analysis.py
+++ inviscid_analysis.py
@@ -11,38 +11,31 @@ sns.set(palette=sns.color_palette("husl"))
 
 ### Set up givens
 
-a = Airfoil("naca4408").repanel(n_points_per_side=50)
-x_panel = a.x()
-y_panel = a.y()
+airfoil = Airfoil("naca4408").repanel(n_points_per_side=50)
 alpha_deg = 5
 
-N = len(a.coordinates)  # number of airfoil nodes
-# N_w = 50  # number of wake nodes
+### Get some basic information about the airfoil
+x_panels = airfoil.x()
+y_panels = airfoil.y()
 
-### Set up optimization framework and unknowns
+N = len(x_panels)  # number of airfoil nodes
+N_w = round(N / 8 + 2)  # number of wake nodes
+
+### Set up optimization/analysis framework and unknowns
 opti = Opti()
 gamma = opti.variable(
     n_vars=N,
     init_guess=0
 )
-# sigma = opti.variable(
-#     n_vars=N,
-#     init_guess=0,
-#     freeze=True,
-# )
 sigma = np.zeros(N)
 
-### Calculate the field points
-x_midpoints = (x_panel[1:] + x_panel[:-1]) / 2
-y_midpoints = (y_panel[1:] + y_panel[:-1]) / 2
-
-### Compute freestream velocities
+### Compute freestream velocity components
 alpha_rad = alpha_deg * pi / 180
 u_freestream = cas.cos(alpha_rad)
 v_freestream = cas.sin(alpha_rad)
 
 
-### Calculate the local velocity at field points
+### Make a function to calculate the local velocity at arbitrary points
 def calculate_velocity(
         x_field,
         y_field,
@@ -53,17 +46,34 @@ def calculate_velocity(
     u_field_induced, v_field_induced = calculate_induced_velocity(
         x_field=x_field,
         y_field=y_field,
-        x_panels=x_panel,
-        y_panels=y_panel,
+        x_panels=x_panels,
+        y_panels=y_panels,
         gamma=gamma,
         sigma=sigma,
         backend=backend
     )
+
+    # u_field_induced_mirror, v_field_induced_mirror = calculate_induced_velocity(
+    #     x_field=x_field,
+    #     y_field=-0.2 - y_field,
+    #     x_panels=x_panel,
+    #     y_panels=y_panel,
+    #     gamma=-gamma,
+    #     sigma=sigma,
+    #     backend=backend
+    # )
+    # u_field_induced += u_field_induced_mirror
+    # v_field_induced += v_field_induced_mirror
+
     u_field = u_field_induced + u_freestream
     v_field = v_field_induced + v_freestream
 
     return u_field, v_field
 
+
+### Compute normal velocities at the middle of each panel
+x_midpoints = (x_panels[1:] + x_panels[:-1]) / 2
+y_midpoints = (y_panels[1:] + y_panels[:-1]) / 2
 
 u_midpoints, v_midpoints = calculate_velocity(
     x_field=x_midpoints,
@@ -73,9 +83,8 @@ u_midpoints, v_midpoints = calculate_velocity(
     backend="casadi"
 )
 
-### Compute normal velocities
-panel_dx = np.diff(x_panel)
-panel_dy = np.diff(y_panel)
+panel_dx = np.diff(x_panels)
+panel_dy = np.diff(y_panels)
 panel_length = (panel_dx ** 2 + panel_dy ** 2) ** 0.5
 
 xp_hat_x = panel_dx / panel_length  # x-coordinate of the xp_hat vector
@@ -96,8 +105,6 @@ opti.subject_to(gamma[0] + gamma[-1] == 0)
 sol = opti.solve()
 gamma = sol.value(gamma)
 sigma = sol.value(sigma)
-u_midpoints = sol.value(u_midpoints)
-v_midpoints = sol.value(v_midpoints)
 
 ### Calculate lift coefficient
 total_vorticity = np.sum(
@@ -107,7 +114,7 @@ total_vorticity = np.sum(
 Cl = 2 * total_vorticity
 print(f"Cl: {Cl}")
 
-### Plot flowfield
+### Plot the flowfield
 fig, ax = plt.subplots(1, 1, figsize=(8, 5), dpi=200)
 
 margin = 0.4
@@ -147,7 +154,7 @@ plt.streamplot(
     cmap=streamplot_colormap.mpl_colormap,
 )
 
-plt.fill(x_panel, y_panel, "k", linewidth=0, zorder=4)
+plt.fill(x_panels, y_panels, "k", linewidth=0, zorder=4)
 CB = plt.colorbar(
     orientation="horizontal",
     shrink=0.8,
@@ -160,7 +167,7 @@ plt.clim(0.7, 1.3)
 plt.gca().set_aspect('equal', adjustable='box')
 plt.xlabel(r"$x/c$")
 plt.ylabel(r"$y/c$")
-plt.title(rf"Flow Field around {a.name} Airfoil at $\alpha = {alpha_deg}\degree$")
+plt.title(rf"Flow Field around {airfoil.name} Airfoil at $\alpha = {alpha_deg}\degree$")
 plt.tight_layout()
 plt.show()
 
